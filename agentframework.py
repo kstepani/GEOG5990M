810ec617bcdbbce1260b1913e5d33d820e23c22b
kstepani <43998661+kstepani@users.noreply.github.com>
2018-12-12 22:13:24 +0000

    added documentation

--- agentframework.py
+++ agentframework.py
@@ -1,12 +1,30 @@
+"""
+This is a module containing the Agent class. This class includes all the properties and behaviour of our agents.
+"""
+
 import random
 class Agent(): 
     #initialising x,y,environment,agents
-    def __init__(self, environment, agents, neighbourhood, y = None, x = None):
+    #the default values of y and x are None
+    def __init__(self, environment, agents, y = None, x = None):
+        """
+        Initialise the agent.
+        
+        environment -- a raster file in which the agents are located
+        agents -- all the agents in the environment
+        neighbourhood -- the surrounding area of the agent 
+        y -- y coordinate of the agent's location
+        x -- x coordinate of the agent's location
+        """
+        
+        self.x=x
+        self.y=y
         self.environment = environment
         self.store = 0 
         self.agents = agents
         self.neighbourhood = neighbourhood
         
+        #if x and y data are missing then randomised values will be given to x and y variables
         if (x == None):
             self._x = random.randint(0,100)
         else:
@@ -19,6 +37,9 @@ class Agent():
     
    #define move method      
     def move(self):
+        """
+        Moves the agent in the environment
+        """
         if random.random() < 0.5:
             self.y = (self.y + 1) % 100
         else:
@@ -30,7 +51,10 @@ class Agent():
             self.x = (self.x - 1) % 100
             
     #define eat method
-    def eat(self): # can you make it eat what is left?
+    def eat(self): 
+        """
+        The Agent eats the environment
+        """
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10
@@ -38,11 +62,17 @@ class Agent():
     
     #define distance function            
     def distance_between(self,agents):
+        """
+        A function which calculates and returns the distance between agents
+        """
         return (((self.x - agents.x)**2) + ((self.y - agents.y)**2))**0.5 
          
                 
     #define share_with_neighbours method
     def share_with_neighbours(self, neighbourhood):
+        """
+        The agent interacts with other agents in its neighbourhood
+        """
         for agent in self.agents:
             dist = self.distance_between(agent) 
             if dist <= neighbourhood:
--- model8.py
+++ model8.py
@@ -1,5 +1,16 @@
+"""
+This is an Agent Based Model which reads some environmental data and builds some agents by downloading data from the web.
+It also makes these agents to move and interact with their environment and with each other. The result is 
+an animation displayed through a GUI. When the model is run, a GUI window should apear and the animation is displayed by clicking
+on the 'model' option(on the menu bar) and then on the 'run model' option.
+"""
+
+
+"""
+Import all the required libraries and files
+"""
 import random
-import operator
+#import operator
 import tkinter
 import matplotlib
 matplotlib.use('TkAgg')
@@ -10,17 +21,35 @@ import matplotlib.animation
 import requests
 import bs4
 
-#downloading y and x data from a webpage
+
+"""
+Set all necessary parameters
+"""
+num_of_agents = 100
+num_of_iterations = 100
+neighbourhood = 20
+agents = []
+
+
+"""
+Download x and y data from a website
+"""
 r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
 content = r.text
 soup = bs4.BeautifulSoup(content, 'html.parser')
 td_ys = soup.find_all(attrs={"class" : "y"})
 td_xs = soup.find_all(attrs={"class" : "x"})
+
+"""
+print the data downloaded just for testing
+"""
 print(td_ys)
 print(td_xs)
 
 
-
+"""
+Create the environment in which agents will interact by reading a csv file
+"""
 #read the csv file and create a 2D List
 with open('in.txt', newline='') as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
@@ -31,37 +60,49 @@ with open('in.txt', newline='') as f:
             rowlist.append(value)
         environment.append(rowlist)
                    
-         
-num_of_agents = 100
-num_of_iterations = 100
-neighbourhood = 20
-agents = []
 
-fig = matplotlib.pyplot.figure(figsize=(7, 7))
-ax = fig.add_axes([0, 0, 1, 1])
-#ax.set_autoscale_on(False)
-
-
+"""
+Initialise the agents
+"""        
 # Make the agents,link them with the environment and the list of agents
 for i in range(num_of_agents):
     y = int(td_ys[i].text)
     x = int(td_xs[i].text)
-    agents.append(agentframework.Agent(environment,agents,neighbourhood, y, x))
-    
+    agents.append(agentframework.Agent(environment,agents, y, x))
+
+
+"""
+Set the GUI window
+"""
+#set the figure
+fig = matplotlib.pyplot.figure(figsize=(7, 7)) 
+ax = fig.add_axes([0, 0, 1, 1])
+#ax.set_autoscale_on(False)
+
+#set the GUI main window
+root = tkinter.Tk() 
+root.wm_title("Model")
 
 carry_on = True
 
+"""
+Create the animation
+"""
 def update(frame_number):
-    
+    """
+    This function updates the display while agents are moving and interacting
+    """
     fig.clear() 
     global carry_on
       
     # Move the agents and make them interact with the environment and with each other
+    random.shuffle(agents)#makes agents to be processed in randomised order
     for i in range(num_of_agents):
         agents[i].move()
         agents[i].eat()
         agents[i].share_with_neighbours(neighbourhood)
 
+    #set a stopping condition for the update of the animation
     if random.random() < 0.1:
         carry_on = False
         print("stopping condition")
@@ -74,6 +115,9 @@ def update(frame_number):
         matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
         
 def gen_function(b = [0]):
+    """
+    A function which stops the iterations of the update function
+    """
     a = 0
     global carry_on #Not actually needed as we're not assigning, but clearer
     while (a < 10) & (carry_on) :
@@ -81,13 +125,17 @@ def gen_function(b = [0]):
         a = a + 1
 
 #animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=10)
+animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
+
 def run():
+    """
+    The function which runs the animation and is connected to the 'Run model' option in the menu bar of the GUI
+    """
     global animation
     animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)                                                       
     canvas.show()
 
-root = tkinter.Tk() 
-root.wm_title("Model")
+
 canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
 canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 menu = tkinter.Menu(root)
